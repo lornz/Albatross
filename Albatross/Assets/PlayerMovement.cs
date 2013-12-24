294de62dd999dc118d80a90c4d2f91627ec8f514
Temophor <Temophor@gmail.com>
2013-12-24 12:12:36 +0100

    stuff

---
+++ Albatross/Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public void ballMovement() {
+		float hMovement = Input.GetAxis ("Horizontal");
+		float vMovement = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (hMovement, 0.0f, vMovement);
+		rigidbody.AddForce (movement);
+	}
+}
