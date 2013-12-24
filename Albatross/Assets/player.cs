6bee0d1593d78fe755ebcf33310a02c9f71247c3
Temophor <Temophor@gmail.com>
2013-12-24 12:11:33 +0100

    player.cs movement ausgelagert

--- Albatross/Assets/player.cs
+++ Albatross/Assets/player.cs
@@ -78,19 +78,4 @@ public class player : MonoBehaviour {
 			syncStartPosition = rigidbody.position;
 		}
 	}
-	
-	void InputMovement()
-	{
-		if (Input.GetKey(KeyCode.W))
-			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
-		
-		if (Input.GetKey(KeyCode.S))
-			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
-		
-		if (Input.GetKey(KeyCode.D))
-			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
-		
-		if (Input.GetKey(KeyCode.A))
-			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
-	}
 }
